ae41a98629a6721cd76394e5054c4a4ee84394af
tschaffter <thomas.schaffter@gmail.com>
2021-06-20 21:26:11 -0700

    Run npm run generate:server:latest

--- server/openapi_server/controllers/tool_controller.py
+++ server/openapi_server/controllers/tool_controller.py
@@ -1,5 +1,6 @@
 from openapi_server.models.tool import Tool  # noqa: E501
 from openapi_server.models.tool_dependencies import ToolDependencies  # noqa: E501
+from openapi_server.models.tool_type import ToolType
 from openapi_server.models.license import License
 
 
@@ -10,15 +11,16 @@ def get_tool():  # noqa: E501
     """
     tool = Tool(
         name="contact-annotator-example",
-        version="1.1.2",
+        version="1.2.0",
         license=License.APACHE_2_0,
         repository="github:nlpsandbox/contact-annotator-example",
-        description="Example implementation of the NLP Sandbox Contact Annotator",
+        description="Example implementation of the NLP Sandbox Contact " +
+                "Annotator API",
         author="NLP Sandbox Team",
         author_email="team@nlpsandbox.io",
         url="https://github.com/nlpsandbox/contact-annotator-example",
-        type="nlpsandbox:contact-annotator",
-        api_version="1.1.2"
+        type=ToolType.CONTACT_ANNOTATOR,
+        api_version="1.2.0"
     )
     return tool, 200
 
